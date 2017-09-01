b2d717125d62e3626e56a88f3078599319757ca1
Brandon Conway <brandoncc@gmail.com>
2017-08-31 20:08:03 -0700

    Add example ui for single page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
- root to: 'home#index'
+  root to: 'home#index'
+
+  get '/ui/all_boards', to: 'ui#all_boards'
+  get '/ui/single_board', to: 'ui#single_board'
 end
