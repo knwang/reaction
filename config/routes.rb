4299e6bd505aec4610a0a271229708c9cab40f96
Brandon Conway <brandoncc@gmail.com>
2017-09-04 15:54:10 -0700

    Add /ui index page

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   get '/ui/all_boards', to: 'ui#all_boards'
   get '/ui/single_board', to: 'ui#single_board'
   get '/ui/create_board', to: 'ui#create_board'
+  get '/ui', to: 'ui#index'
 end
