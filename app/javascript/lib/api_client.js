7cf7133e0d58ab4431cf77a74843984e86cfb87d
Brandon Conway <brandoncc@gmail.com>
2017-09-04 15:02:32 -0700

    Check for successful api response

--- app/javascript/lib/api_client.js
+++ app/javascript/lib/api_client.js
@@ -2,9 +2,22 @@ import axios from 'axios';
 
 const BOARDS_INDEX_URL = '/api/boards';
 
+function checkStatus(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return response;
+  } else {
+    const error = new Error(`HTTP Error ${response.statusText}`);
+    error.status = response.statusText;
+    error.response = response;
+    console.log(error); // eslint-disable-line no-console
+    throw error;
+  }
+}
+
 const apiClient = {
   getBoards: function(callback) {
     return axios.get(BOARDS_INDEX_URL)
+      .then(checkStatus)
       .then(response => response.data)
       .then(callback);
   }
