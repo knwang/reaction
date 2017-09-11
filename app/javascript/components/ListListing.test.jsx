e6e675d4b2e20c4536e8a3288d886eb8c2e22fa7
Brandon Conway <brandoncc@gmail.com>
2017-09-11 15:47:07 -0700

    Add ListListing tests

---
+++ app/javascript/components/ListListing.test.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import ListListing from './ListListing';
+import List from './List';
+
+describe("ListListing", () => {
+  let wrapper;
+
+  it("displays no lists", () => {
+    wrapper = shallow(<ListListing lists={[]} />);
+
+    expect(
+      wrapper.containsMatchingElement(<List />)
+    ).toEqual(false);
+  });
+
+  it("displays one list", () => {
+    const lists = [{ id: 1, title: "My list" }];
+    wrapper = shallow(<ListListing lists={lists} />);
+
+    expect(
+      wrapper.containsMatchingElement(<List list={lists[0]} />)
+    ).toEqual(true);
+  });
+
+  it("it displays more than one list", () => {
+    const lists = [{ id: 1, title: "My list" },
+                   { id: 2, title: "My other list" }];
+
+    wrapper = shallow(<ListListing lists={lists} />);
+
+    expect(
+      wrapper.containsAllMatchingElements([
+        <List list={lists[0]} />,
+        <List list={lists[1]} />
+      ])
+    ).toEqual(true);
+  });
+
+  it("displays a new list button", () => {
+    wrapper = shallow(<ListListing lists={[]} />);
+
+    expect(
+      wrapper.find('.new-list').length
+    ).toEqual(1);
+  })
+});
