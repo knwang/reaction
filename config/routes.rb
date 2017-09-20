b01e544032e0f099f197ad799e07db9f1f636d29
Brandon Conway <brandoncc@gmail.com>
2017-09-20 13:56:00 -0700

    Add card ui template

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,6 @@ Rails.application.routes.draw do
   get '/ui/all_boards', to: 'ui#all_boards'
   get '/ui/single_board', to: 'ui#single_board'
   get '/ui/create_board', to: 'ui#create_board'
+  get '/ui/card', to: 'ui#card'
   get '/ui', to: 'ui#index'
 end
