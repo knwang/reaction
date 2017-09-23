a063a96b1949b5a6658cd6751d4355a30b69317d
Brandon Conway <brandoncc@gmail.com>
2017-09-23 14:43:08 -0700

    BoardsDashboard is responsible for loading all boards

--- app/javascript/components/BoardsDashboard.jsx
+++ app/javascript/components/BoardsDashboard.jsx
@@ -1,21 +1,35 @@
+import React from 'react';
 import { connect } from 'react-redux';
 import BoardsDashboardDisplay from './BoardsDashboardDisplay';
+import PropTypes from 'prop-types';
 
-function mapDashboardStateToProps(state) {
-  return {
-    boards: state.boards
+import * as actions from '../actions/BoardActions';
+
+class BoardsDashboard extends React.Component {
+  static contextTypes = {
+    store: PropTypes.object.isRequired
   };
+
+  componentDidMount() {
+    const store = this.context.store;
+    this.unsubscribe = store.subscribe(() => this.forceUpdate());
+    store.dispatch(actions.fetchBoards());
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  allBoards = () => {
+    const store = this.context.store;
+    return store.getState().boards;
+  }
+
+  render() {
+    return (
+      <BoardsDashboardDisplay boards={this.allBoards()} />
+    )
+  }
 }
-
-function mapDashboardDispatchToProps(dispatch) {
-  return {
-
-  };
-}
-
-const BoardsDashboard = connect(
-  mapDashboardStateToProps,
-  mapDashboardDispatchToProps
-)(BoardsDashboardDisplay);
 
 export default BoardsDashboard;
