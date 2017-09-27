37a28bd19a9c7e0d3f4b778cf05baf45668333eb
Brandon Conway <brandoncc@gmail.com>
2017-09-26 18:57:30 -0700

    Add default position to lists and make it required

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170926230213) do
+ActiveRecord::Schema.define(version: 20170927015636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 20170926230213) do
     t.text "description"
     t.string "labels", default: [], array: true
     t.integer "list_id"
-    t.float "position"
+    t.float "position", default: 65535.0, null: false
   end
 
   create_table "lists", force: :cascade do |t|
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 20170926230213) do
     t.integer "board_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.float "position", default: 0.0, null: false
+    t.float "position", default: 65535.0, null: false
   end
 
 end
