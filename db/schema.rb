38b3d1b013351113607d04bdf1977a077c934ec4
Brandon Conway <brandoncc@gmail.com>
2017-10-01 11:34:40 -0700

    Add timestamps to cards and comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170930204017) do
+ActiveRecord::Schema.define(version: 20171001182811) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,11 +28,15 @@ ActiveRecord::Schema.define(version: 20170930204017) do
     t.integer "list_id"
     t.float "position", default: 65535.0, null: false
     t.boolean "archived", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "comments", force: :cascade do |t|
     t.text "text", default: ""
     t.integer "card_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["card_id"], name: "index_comments_on_card_id"
   end
 
