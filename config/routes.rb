12674314289248416253fc6a1e968bbfa3edffa9
Brandon Conway <brandoncc@gmail.com>
2017-10-01 19:27:26 -0700

    Add labels static UI

--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,6 @@ Rails.application.routes.draw do
   get '/ui/card_editing_description', to: 'ui#card_editing_description'
   get '/ui/card_archived', to: 'ui#card_archived'
   get '/ui/due_date_popover', to: 'ui#due_date_popover'
+  get '/ui/labels_popover', to: 'ui#labels_popover'
   get '/ui', to: 'ui#index'
 end
